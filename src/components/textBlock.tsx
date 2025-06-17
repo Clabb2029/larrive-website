640ff0616b193e18eb2660f200a2b4d7b09592cf
Clara Slys <slys.clara89000@gmail.com>
2025-06-17 21:48:14 +0200

    generic text block implemented

---
+++ src/components/textBlock.tsx
@@ -0,0 +1,25 @@
+import { Scissors } from "lucide-react";
+
+export default function TextBlock({ placement, children }: { placement?: 'top-left' | 'top-right' | 'bottom-left' | 'bottom-right' | 'right' | 'left', children?: React.ReactNode }) {
+    return (
+        <div className="relative outline-1 outline-base-300 outline-dashed bg-base-200 m-4 p-4">
+
+            { placement && (
+                <div
+                    className={`
+                        absolute text-base-300 p-1
+                        ${ placement === 'top-left' ? '-top-3 left-1' :
+                        placement === 'top-right' ? '-top-3 right-1' :
+                        placement === 'bottom-left' ? '-bottom-3 left-1' :
+                        placement === 'bottom-right' ? '-bottom-3 right-1' :
+                        placement === 'left' ? '-left-3 top-1/2 transform -translate-y-1/2 rotate-90' :
+                        placement === 'right' ? '-right-3 top-1/2 transform -translate-y-1/2 -rotate-90' : '' }
+                    `}
+                >
+                    <Scissors size={15}/>
+                </div>
+            )}            
+            <p>{ children }</p>
+        </div>
+    );
+}
