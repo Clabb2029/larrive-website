06bbbf9c4b50a496eba1bd4593aec333574bc7b7
Clara Slys <slys.clara89000@gmail.com>
2025-06-18 15:07:55 +0200

    footer font size modifications

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,13 +4,12 @@ import Image from "next/image";
 export default function Footer() {
 
     return (
-        <footer className="bg-base-content text-base-100">
-            <div className="container px-6 py-6 mx-auto">
+        <footer className="bg-base-content text-base-100 text-xs">
+            <div className="container px-6 py-4 mx-auto">
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-6 sm:gap-y-10">
                     <Image src="/logo.png" height={60} width={60} alt="Website logo" className="col-span-1 filter invert brightness-0 mx-auto my-auto"/>
-                    
-                    <div className="flex flex-col lg:flex-row items-stretch  col-span-1 lg:col-span-4 w-fit sm:w-full mx-auto divide-y lg:divide-y-0 divide-base-100 lg:divide-x">
-                        <div className="flex flex-row items-center lg:justify-center flex-1 pe-8 py-2">
+                    <div className="flex flex-col lg:flex-row items-stretch col-span-1 lg:col-span-4 w-fit lg:w-full mx-auto divide-y lg:divide-y-0 divide-base-100 lg:divide-x">
+                        <div className="flex flex-row items-center lg:justify-center flex-1 lg:pe-8 py-2">
                             <MapPin size={30} className="me-3"/>
                             <div>
                                 <p className="font-bold">ADRESSE</p>
@@ -34,7 +33,7 @@ export default function Footer() {
                     </div>
                 </div>
             </div>
-            <p className="p-8 md:p-4 text-center">{new Date().getFullYear()} Atelier Jean-Claude Larrivé. Tous droits réservés.</p>
+            <p className="p-4 text-center">{new Date().getFullYear()}, Atelier Jean-Claude Larrivé. Tous droits réservés.</p>
         </footer>
     )
 }
