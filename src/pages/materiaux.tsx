1721e78326d6e82744e68c063c2f76e6cfdb52fa
Clara Slys <slys.clara89000@gmail.com>
2025-06-24 02:11:33 +0200

    add pictograms to materials page

--- src/pages/materiaux.tsx
+++ src/pages/materiaux.tsx
@@ -3,6 +3,13 @@ import TextBlock from "@/components/textBlock";
 
 export default function Materiaux() {
 
+    const pictos = [
+        { src: "green-line.png", alt: "Fils recyclés issus de bouteilles en plastique usagées, certifiés traçables GRS" },
+        { src: "h2oh.png", alt: "Tissus permettent de nettoyer facilement les taches ménagères courantes avec de l'eau" },
+        { src: "pet-friendly.png", alt: "Tissus respectueux des animaux et résistants aux griffures et aux accrocs" },
+        { src: "rpet.png", alt: "Fabrication à partir d'emballages en PET recyclé" }
+    ];
+
     const materials = [
         { src: "materiaux-1.jpg", alt: "Tissu de tapisserie coloré et fleuri avec des oiseaux" },
         { src: "materiaux-2.jpg", alt: "Tissu de tapisserie coloré avec des formes arrondies" },
@@ -11,7 +18,8 @@ export default function Materiaux() {
         { src: "materiaux-5.jpg", alt: "Tissu de tapisserie bleu avec des formes géométriques" },
         { src: "materiaux-6.jpeg", alt: "Échantillons de tissus de tapisserie présentant différents motifs" },
         { src: "materiaux-7.jpg", alt: "Échantillons de tissus de tapisserie unis et à motifs" },
-        { src: "materiaux-8.jpg", alt: "Tissu de tapisserie coloré avec des formes géométriques" }
+        { src: "materiaux-8.jpg", alt: "Tissu de tapisserie coloré avec des formes géométriques" },
+        { src: "materiaux-9.jpg", alt: "Tissu de tapisserie coloré avec des formes géométriques" }
     ]
 
     const largeScreenStyle = [
@@ -20,9 +28,10 @@ export default function Materiaux() {
         { position: "sm:top-[30%] sm:right-[2%] lg:top-[40%] z-2", rotation: "rotate-10" },
         { position: "sm:top-[50%] lg:top-[45%] sm:right-[17%] lg:right-[15%]", rotation: "-rotate-10" },
         { position: "sm:top-[55%] lg:top-[40%] sm:right-[32%] lg:right-[30%] z-1", rotation: "rotate-10" },
-        { position: "sm:top-[50%] lg:top-[45%] sm:right-[50%] lg:right-[45%]", rotation: "-rotate-5" },
-        { position: "sm:top-[55%] lg:top-[40%] sm:right-[65%] lg:right-[60%]", rotation: "rotate-10" },
-        { position: "sm:top-[50%] lg:top-[45%] sm:right-[80%] lg:right-[75%]", rotation: "-rotate-10" }
+        { position: "sm:top-[50%] lg:top-[45%] sm:right-[45%] lg:right-[45%]", rotation: "-rotate-5" },
+        { position: "sm:top-[55%] lg:top-[40%] sm:right-[60%] lg:right-[60%]", rotation: "rotate-10" },
+        { position: "sm:top-[50%] lg:top-[45%] sm:right-[75%] lg:right-[75%]", rotation: "-rotate-10" },
+        { position: "sm:top-[70%] lg:top-[78%] sm:right-[82%] lg:right-[80%]", rotation: "rotate-10" }
     ];
 
     const smallScreenStyle = [
@@ -33,6 +42,7 @@ export default function Materiaux() {
         { position: "top-[55%] left-[2%] z-3", rotation: "-rotate-5" },
         { position: "top-[25%] left-[52%] z-3", rotation: "rotate-10" },
         { position: "top-[50%] left-[70%] z-3", rotation: "-rotate-10" },
+        { position: "top-[55%] left-[35%] z-3", rotation: "rotate-10" },
         { position: "top-[55%] left-[35%] z-3", rotation: "rotate-10" }
     ];
 
@@ -55,7 +65,7 @@ export default function Materiaux() {
                 key={index}
                 src={`/materiaux/${material.src}`}
                 alt={material.alt}
-                className={`absolute ${style[index].position} ${style[index].rotation} w-[35%] sm:w-[20%] lg:w-[18%] xl:w-[15%] object-cover border-3 border-primary shadow-lg transition-all duration-300`}
+                className={`absolute ${style[index].position} ${style[index].rotation} w-[35%] sm:w-[18%] lg:w-[18%] xl:w-[15%] object-cover border-3 border-primary shadow-lg transition-all duration-300`}
             />
         ))
     }
@@ -64,14 +74,25 @@ export default function Materiaux() {
         <PageLayout className="p-10">
             <div className="w-full">
                 <h1 className="text-4xl parisienne-regular text-primary">Les matériaux utilisés</h1>
-                <div className="relative w-full h-[500px] lg:h-[600px] mt-10 hidden sm:block">
+                <div className="relative w-full h-[500px] lg:h-[550px] xl:h-[500px] mt-10 hidden sm:block">
                     {getTextBlock()}
                     {getImageList(true)}
                 </div>
                 <div className="block sm:hidden mt-10">
                     {getTextBlock()}
-                    <div className="w-full h-[400px] relative">
+                    <div className="w-full h-[100vw] relative">
                         {getImageList(false)}
+                    </div>
+                </div>
+                <div className="card card-border border-base-300 bg-base-200 rounded-md shadow-2xl h-fit w-fit mx-auto mt-[1vw] sm:mt-0">
+                    <div className="card-body flex flex-row py-4 gap-4">
+                        {
+                            pictos.map((picto, index) => (
+                                <div key={index} className="sm:tooltip sm:tooltip-primary inline-block mr-2" data-tip={picto.alt}>
+                                    <img src={`/pictos/${picto.src}`} alt={picto.alt} className="w-[12vh]" />
+                                </div>
+                            ))
+                        }
                     </div>
                 </div>
             </div>
