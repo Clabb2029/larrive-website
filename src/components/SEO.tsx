c27eb15841dddec854355fb09378483f8a1dd8bc
Clara Slys <slys.clara89000@gmail.com>
2025-06-29 17:01:23 +0200

    add link canonical

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -1,15 +1,15 @@
-// components/SEO.js
 import Head from 'next/head'
 
 export default function SEO({ title, description, keywords }: { title: string, description: string, keywords: string }) {
     return (
         <Head>
+            <meta charSet="utf-8" />
             <title>{title}</title>
             <meta name="description" content={description} />
             <meta name="keywords" content={keywords} />
             <meta name="author" content="Jean-Claude Larrivé" />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <meta charSet="utf-8" />
+            <link rel="canonical" href="https://www.atelier-larrive.fr" />
         </Head>
     )
 }
